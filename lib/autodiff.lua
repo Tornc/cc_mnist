0b07cb1840d8dbdf33925dda2223798c09e042b0
Tornc <ttlam04@gmail.com>
2026-01-09 22:13:17 +0100

    tmp

--- lib/autodiff.lua
+++ lib/autodiff.lua
@@ -11,53 +11,39 @@ local autodiff = {}
 --[[ ENUMS, UTILITY, CLASSES ]]
 
 local MV_FLAG = { --- @enum ModelVariableFlag
-    NONE = 1,
-
-    REQUIRES_GRAD = 2,
-    PARAMETER = 3,
-    INPUT = 4,
-    OUTPUT = 5,
-    DESIRED_OUTPUT = 6,
-    COST = 7,
+    NONE           = bit32.lshift(0, 0),
+    REQUIRES_GRAD  = bit32.lshift(1, 0),
+    PARAMETER      = bit32.lshift(1, 1),
+    INPUT          = bit32.lshift(1, 2),
+    OUTPUT         = bit32.lshift(1, 3),
+    DESIRED_OUTPUT = bit32.lshift(1, 4),
+    COST           = bit32.lshift(1, 5),
 }
 local MV_OP = { --- @enum ModelVariableOperation
-    NULL = 1,
-    CREATE = 2,
-
-    _UNARY_START = 3,
-
-    RELU = 4,
-    SOFTMAX = 5,
-
-    _BINARY_START = 6,
-
-    ADD = 7,
-    SUB = 8,
-    MATMUL = 9,
-    CROSS_ENTROPY = 10,
+    NULL          = 0000,
+    CREATE        = 0001,
+    _UNARY_START  = 1000, -- Demarcator
+    RELU          = 1001,
+    SOFTMAX       = 1002,
+    _BINARY_START = 2000, -- Demarcator
+    ADD           = 2001,
+    SUB           = 2002,
+    MATMUL        = 2003,
+    CROSS_ENTROPY = 2004,
 }
 
 local MV_MAX_INPUTS = 2
+
+--- @param op ModelVariableOperation
+--- @return integer
 local MV_NUM_INPUTS = function(op)
     if op < MV_OP._UNARY_START then return 0 end
     if op < MV_OP._BINARY_START then return 1 end
     return 2
 end
 
---- @TODO: not too much a fan of flag tables anymore...
---- @param flags table<ModelVariableFlag, boolean>
---- @param flag ModelVariableFlag
---- @return table<ModelVariableFlag, boolean>
-local add_flag = function(flags, flag)
-    local tbl = {}
-    for k, _ in pairs(flags) do tbl[k] = true end
-    tbl[flag] = true
-    return tbl
-end
-
---- @TODO: make some of these params optional.
 --- @param index integer
---- @param flags table<ModelVariableFlag, boolean>
+--- @param flags integer
 --- @param val Matrix2d
 --- @param grad Matrix2d?
 --- @param op ModelVariableOperation
@@ -77,13 +63,18 @@ local function model_var(index, flags, val, grad, op, inputs)
     return self
 end
 
+--- @TODO: make this into a class
+local function model_program2()
+end
+
+--- @TODO: make this into a class
 --- @param num_vars integer
---- @param input ModelVariable
---- @param output ModelVariable
---- @param desired_output ModelVariable
---- @param cost ModelVariable
---- @param forward_prog table<ModelVariable>
---- @param cost_prog table<ModelVariable>
+--- @param input ModelVariable?
+--- @param output ModelVariable?
+--- @param desired_output ModelVariable?
+--- @param cost ModelVariable?
+--- @param forward_prog table<ModelVariable|nil>
+--- @param cost_prog table<ModelVariable|nil>
 --- @return ModelContext
 local function model_context(num_vars, input, output, desired_output, cost, forward_prog,
                              cost_prog)
@@ -99,6 +90,21 @@ local function model_context(num_vars, input, output, desired_output, cost, forw
     return self
 end
 
+--- @return ModelContext2
+function model_context2()
+    --- @class ModelContext2
+    local self = {}
+    self.num_vars = 0
+    self.input = nil          --- @type ModelVariable?
+    self.output = nil         --- @type ModelVariable?
+    self.desired_output = nil --- @type ModelVariable?
+    self.cost = nil           --- @type ModelVariable?
+    self.forward_prog = {}    --- @type table<ModelVariable|nil>
+    self.cost_prog = {}       --- @type table<ModelVariable|nil>
+
+    return self
+end
+
 --- @param train_images Matrix2d
 --- @param train_labels Matrix2d
 --- @param test_images Matrix2d
@@ -107,8 +113,8 @@ end
 --- @param batch_size integer
 --- @param learning_rate number
 --- @return ModelTrainingDescription
-local function model_training_desc(train_images, train_labels, test_images, test_labels,
-                                   epochs, batch_size, learning_rate)
+function autodiff.model_training_desc(train_images, train_labels, test_images, test_labels,
+                                      epochs, batch_size, learning_rate)
     --- @class ModelTrainingDescription
     local self = {}
     self.train_images = train_images
@@ -123,22 +129,22 @@ end
 
 --[[ MODEL VARIABLE FACTORIES ]]
 
---- @param model ModelContext This may get modified.
+--- @param model ModelContext This will get modified.
 --- @param rows integer
 --- @param cols integer
---- @param flags table<ModelVariableFlag, boolean>
+--- @param flags integer
 --- @return ModelVariable
-local function mv_create(model, rows, cols, flags)
+function autodiff.mv_create(model, rows, cols, flags)
     local out = model_var(
-        model.num_vars + 1,
+        model.num_vars,
         flags,
         matrix2d.fill(0, rows, cols),
-        flags[MV_FLAG.REQUIRES_GRAD] and matrix2d.fill(0, rows, cols) or nil,
+        bit32.btest(flags, MV_FLAG.REQUIRES_GRAD) and matrix2d.fill(0, rows, cols) or nil,
         MV_OP.CREATE,
         {}
     )
+    model.num_vars = model.num_vars + 1
 
-    --- @TODO: this is not clean ngl.
     if flags[MV_FLAG.INPUT] then
         if model.input then error("Input already set!", 2) end
         model.input = out
@@ -163,15 +169,14 @@ end
 --- @param input ModelVariable
 --- @param rows integer
 --- @param cols integer
---- @param flags table<ModelVariableFlag, boolean>
+--- @param flags integer
 --- @param op ModelVariableOperation
 --- @return ModelVariable
 local function _mv_unary_impl(model, input, rows, cols, flags, op)
-    local _flags = flags
-    if input.flags[MV_FLAG.REQUIRES_GRAD] then
-        _flags = add_flag(flags, MV_FLAG.REQUIRES_GRAD)
+    if bit32.btest(input.flags, MV_FLAG.REQUIRES_GRAD) then
+        flags = bit32.bor(flags, MV_FLAG.REQUIRES_GRAD)
     end
-    local out = mv_create(model, rows, cols, _flags)
+    local out = autodiff.mv_create(model, rows, cols, flags)
     out.op = op
     out.inputs[1] = input
     return out
@@ -182,15 +187,16 @@ end
 --- @param b ModelVariable
 --- @param rows integer
 --- @param cols integer
---- @param flags table<ModelVariableFlag, boolean>
+--- @param flags integer
 --- @param op ModelVariableOperation
 --- @return ModelVariable
 local function _mv_binary_impl(model, a, b, rows, cols, flags, op)
-    local _flags = flags
-    if a.flags[MV_FLAG.REQUIRES_GRAD] or b.flags[MV_FLAG.REQUIRES_GRAD] then
-        _flags = add_flag(flags, MV_FLAG.REQUIRES_GRAD)
+    if bit32.btest(a.flags, MV_FLAG.REQUIRES_GRAD) or
+        bit32.btest(b.flags, MV_FLAG.REQUIRES_GRAD)
+    then
+        flags = bit32.bor(flags, MV_FLAG.REQUIRES_GRAD)
     end
-    local out = mv_create(model, rows, cols, _flags)
+    local out = autodiff.mv_create(model, rows, cols, flags)
     out.op = op
     out.inputs[1] = a
     out.inputs[2] = b
@@ -199,17 +205,17 @@ end
 
 --- @param model ModelContext
 --- @param input ModelVariable
---- @param flags table<ModelVariableFlag, boolean>
+--- @param flags integer
 --- @return ModelVariable
-local function mv_relu(model, input, flags)
+function autodiff.mv_relu(model, input, flags)
     return _mv_unary_impl(model, input, input.val.rows, input.val.cols, flags, MV_OP.RELU)
 end
 
 --- @param model ModelContext
 --- @param input ModelVariable
---- @param flags table<ModelVariableFlag, boolean>
+--- @param flags integer
 --- @return ModelVariable
-local function mv_softmax(model, input, flags)
+function autodiff.mv_softmax(model, input, flags)
     return _mv_unary_impl(model, input, input.val.rows, input.val.cols, flags, MV_OP.SOFTMAX)
 end
 
@@ -217,7 +223,7 @@ end
 --- @param a ModelVariable
 --- @param b ModelVariable
 --- @return ModelVariable
-local function mv_add(model, a, b, flags)
+function autodiff.mv_add(model, a, b, flags)
     if a.val.rows ~= b.val.rows then error("Row mismatch!", 2) end
     if a.val.cols ~= b.val.cols then error("Column mismatch!", 2) end
     return _mv_binary_impl(model, a, b, a.val.rows, a.val.cols, flags, MV_OP.ADD)
@@ -227,7 +233,7 @@ end
 --- @param a ModelVariable
 --- @param b ModelVariable
 --- @return ModelVariable
-local function mv_sub(model, a, b, flags)
+function autodiff.mv_sub(model, a, b, flags)
     if a.val.rows ~= b.val.rows then error("Row mismatch!", 2) end
     if a.val.cols ~= b.val.cols then error("Column mismatch!", 2) end
     return _mv_binary_impl(model, a, b, a.val.rows, a.val.cols, flags, MV_OP.SUB)
@@ -237,7 +243,7 @@ end
 --- @param a ModelVariable
 --- @param b ModelVariable
 --- @return ModelVariable
-local function mv_matmul(model, a, b, flags)
+function autodiff.mv_matmul(model, a, b, flags)
     if a.val.cols ~= b.val.rows then error("Column-row mismatch!", 2) end
     return _mv_binary_impl(model, a, b, a.val.rows, b.val.cols, flags, MV_OP.MATMUL)
 end
@@ -246,7 +252,7 @@ end
 --- @param p ModelVariable
 --- @param q ModelVariable
 --- @return ModelVariable
-local function mv_cross_entropy(model, p, q, flags)
+function autodiff.mv_cross_entropy(model, p, q, flags)
     if p.val.rows ~= q.val.rows then error("Row mismatch!", 2) end
     if p.val.cols ~= q.val.cols then error("Column mismatch!", 2) end
     return _mv_binary_impl(model, p, q, p.val.rows, p.val.cols, flags, MV_OP.CROSS_ENTROPY)
@@ -258,7 +264,7 @@ end
 --- @param model ModelContext
 --- @param out_var ModelVariable
 --- @return table<ModelVariable>
-local function model_program_create(model, out_var)
+local function model_prog_create(model, out_var)
     return error("Not implemented!", 2)
 end
 
@@ -270,22 +276,32 @@ local function model_prog_compute_grads(prog)
     return error("Not implemented!", 2)
 end
 
---- @TODO: ?????????????????????
+--- @TODO: This should be inside of a class!
 --[[ MODEL CONTEXT FUNCTIONS ]]
 
-local function model_create()
-
+--- @return ModelContext
+function autodiff.model_create()
+    return model_context(
+        0, nil, nil, nil, nil, {}, {}
+    )
 end
 
-local function model_compile()
-
+--- @param model ModelContext This will get modified.
+function autodiff.model_compile(model)
+    if not model.output then error("Model has no output!", 2) end
+    if not model.cost then error("Model has no cost!", 2) end
+    model.forward_prog = model_prog_create(model, model.output)
+    model.cost_prog = model_prog_create(model, model.cost)
 end
 
-local function model_feed_forward()
-
+--- @param model ModelContext This will get modified.
+local function model_feed_forward(model)
+    model_prog_compute(model.forward_prog)
 end
 
-local function model_train()
+--- @param model ModelContext
+--- @param training_desc ModelTrainingDescription
+local function model_train(model, training_desc)
 
 end
 
